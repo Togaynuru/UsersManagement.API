577ec454ae9ff22cc397db6695abefaff3ecb5ac
Togaynuru <togaynuru@hotmail.com>
2021-12-03 14:32:40 +0300

    Add project files.

---
+++ UsersManagement.API/Controllers/UsersController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UsersManagement.API.Fake;
+using UsersManagement.API.Models;
+
+namespace UsersManagement.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private List<User> _users = FakeData.GetUsers(200);
+
+        #region HttpGet
+        public List<User> Get()
+        {
+            return _users;
+        }
+
+
+        [HttpGet("{id}")]
+        public User Get(int id)
+        {
+            var user = _users.FirstOrDefault(x => x.Id == id);
+            return user;
+        }
+        #endregion
+
+        #region HttpPost
+        [HttpPost]
+        public User Post([FromBody] User user)
+        {
+            _users.Add(user);
+            return user;
+        }
+        #endregion
+
+        #region HttpPut
+        [HttpPut]
+        public User Put([FromBody] User user)
+        {
+            var editedUser = _users.FirstOrDefault(x => x.Id == user.Id);
+            editedUser.FirstName = user.FirstName;
+            editedUser.LastName = user.LastName;
+            editedUser.Adress = user.Adress;
+
+            return user;
+        }
+        #endregion
+
+        #region HttpDelete
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            var deletedUser = _users.FirstOrDefault(x => x.Id == id);
+            _users.Remove(deletedUser);
+
+        }
+        #endregion
+
+
+
+
+    }
+}
